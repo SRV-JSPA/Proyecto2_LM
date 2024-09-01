8aa2e880b8d4791ea021e4caff3cffff751f41dd
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2024-09-01 00:18:32 -0600

    Clase Formula

--- problema1.py
+++ problema1.py
@@ -18,3 +18,29 @@ class Clause:
         return any((not literal.negated and valuation[literal.variable]) or
                    (literal.negated and not valuation[literal.variable]) for literal in self.literals)
 
+class Formula:
+    def __init__(self, clauses):
+        self.clauses = clauses  # lista de Cláusulas
+
+    def is_satisfiable(self):
+        # Extraer todas las variables de la fórmula
+        variables = set()
+        for clause in self.clauses:
+            for literal in clause.literals:
+                variables.add(literal.variable)
+
+        # Generar todas las asignaciones posibles
+        n = len(variables)
+        all_assignments = list(itertools.product([False, True], repeat=n))
+
+        # Probar cada asignación
+        for assignment in all_assignments:
+            # Crear un diccionario que asocia variables con su valor de verdad
+            valuation = dict(zip(variables, assignment))
+
+            # Verificar si la fórmula es satisfacible bajo la asignación actual
+            if all(clause.is_satisfied(valuation) for clause in self.clauses):
+                return True, valuation
+
+        return False, None
+
