747fb691ce9bc33190c5b85feb02267f7c589a52
Mauricio Julio Rodrigo Lemus Guzmán <93178012+Rodlemus03@users.noreply.github.com>
2024-09-01 00:18:16 -0600

    Clase Clausula

--- problema1.py
+++ problema1.py
@@ -9,3 +9,12 @@ class Literal:
         return f'¬{self.variable}' if self.negated else str(self.variable)
 
 
+class Clause:
+    def __init__(self, literals):
+        self.literals = literals  # lista de Literales
+
+    def is_satisfied(self, valuation):
+        # Verificar si al menos un literal en la cláusula es verdadero
+        return any((not literal.negated and valuation[literal.variable]) or
+                   (literal.negated and not valuation[literal.variable]) for literal in self.literals)
+
